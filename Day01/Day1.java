259322c1e3b4316e4a7b1a0876c1a5f9d873239a
carmocca <carlossmocholi@hotmail.com>
2016-12-04 01:55:00 +0100

    Fixed indentation

--- Day01/Day1.java
+++ Day01/Day1.java
@@ -5,68 +5,66 @@ import java.nio.file.Files;
 import java.io.IOException;
 
 public class Day1 {
-	public static void main (String[] args) {
-		String input = null;
-		try {
-			input = new String(Files.readAllBytes(Paths.get("input.txt")));
-		} catch (IOException e) {
-			System.out.println("Error reading file");
-			System.exit(0);
-		}
-		String regex = "([L|R])(\\d+)";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(input);
+    public static void main (String[] args) {
+        String input = null;
+        try {
+            input = new String(Files.readAllBytes(Paths.get("input.txt")));
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+            System.exit(0);
+        }
+        String regex = "([L|R])(\\d+)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
 
-		int x = 0;
-		int y = 0;
-		String lastDirection = "U";
-		String newDirection;
-		while (matcher.find()) {
-			newDirection = matcher.group(1);
-			int distance = Integer.parseInt(matcher.group(2));
-			switch (lastDirection){
-				case "R":	if (newDirection.equals("R")) {
-								y -= distance;
-								lastDirection = "D";
-							} else {
-								y += distance;
-								lastDirection = "U";
-							}
-							break;
-				case "L":	if (newDirection.equals("R")) {
-								y += distance;
-								lastDirection = "U";
-							} else {
-								y -= distance;
-								lastDirection = "D";
-							}
-							break;
-				case "U":	if (newDirection.equals("R")) {
-								x += distance;
-								lastDirection = "R";
-							} else {
-								x -= distance;
-								lastDirection = "L";
-							}
-							break;
-				case "D":	if (newDirection.equals("R")) {
-								x -= distance;
-								lastDirection = "L";
-							} else {
-								x += distance;
-								lastDirection = "R";
-							}
-							break;
-				default:	System.out.println("Error reading file");
-							break;		
-			}
-		}
-		System.out.println("Shortest path to the destination: " + distance(x, 0, y, 0));
-	}
+        int x = 0;
+        int y = 0;
+        String lastDirection = "U";
+        String newDirection;
+        while (matcher.find()) {
+            newDirection = matcher.group(1);
+            int distance = Integer.parseInt(matcher.group(2));
+            switch (lastDirection){
+                case "R":   if (newDirection.equals("R")) {
+                                y -= distance;
+                                lastDirection = "D";
+                            } else {
+                                y += distance;
+                                lastDirection = "U";
+                            }
+                            break;
+                case "L":   if (newDirection.equals("R")) {
+                                y += distance;
+                                lastDirection = "U";
+                            } else {
+                                y -= distance;
+                                lastDirection = "D";
+                            }
+                            break;
+                case "U":   if (newDirection.equals("R")) {
+                                x += distance;
+                                lastDirection = "R";
+                            } else {
+                                x -= distance;
+                                lastDirection = "L";
+                            }
+                            break;
+                case "D":   if (newDirection.equals("R")) {
+                                x -= distance;
+                                lastDirection = "L";
+                            } else {
+                                x += distance;
+                                lastDirection = "R";
+                            }
+                            break;
+                default:    System.out.println("Error reading file");
+                            break;      
+            }
+        }
+        System.out.println("Shortest path to the destination: " + distance(x, 0, y, 0));
+    }
 
-	public static int distance(int x1, int x0, int y1, int y0){
-		return Math.abs(x1 - x0) + Math.abs(y1 - y0);
-	}
+    public static int distance(int x1, int x0, int y1, int y0){
+        return Math.abs(x1 - x0) + Math.abs(y1 - y0);
+    }
 }
-
-
--- Day01/Day1_2.java
+++ Day01/Day1_2.java
@@ -7,78 +7,78 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Day1_2 {
-	public static void main (String[] args) {
-		String input = null;
-		try {
-			input = new String(Files.readAllBytes(Paths.get("input.txt")));
-		} catch (IOException e) {
-			System.out.println("Error reading file");
-			System.exit(0);
-		}
-		String regex = "([L|R])(\\d+)";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(input);
+    public static void main (String[] args) {
+        String input = null;
+        try {
+            input = new String(Files.readAllBytes(Paths.get("input.txt")));
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+            System.exit(0);
+        }
+        String regex = "([L|R])(\\d+)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
 
-		int x = 0;
-		int y = 0;
-		ArrayList<Line> array = new ArrayList<>();
-		Point origin = new Point(0, 0);
-		Point lastPoint = origin;
-		boolean found = false;
+        int x = 0;
+        int y = 0;
+        ArrayList<Line> array = new ArrayList<>();
+        Point origin = new Point(0, 0);
+        Point lastPoint = origin;
+        boolean found = false;
 
-		String lastDirection = "U";
-		while (matcher.find() && !found) {
-			String newDirection = matcher.group(1);
-			int distance = Integer.parseInt(matcher.group(2));
-			switch (lastDirection){
-				case "R":	if (newDirection.equals("R")) {
-								y -= distance;
-								lastDirection = "D";
-							} else {
-								y += distance;
-								lastDirection = "U";
-							}
-							break;
-				case "L":	if (newDirection.equals("R")) {
-								y += distance;
-								lastDirection = "U";
-							} else {
-								y -= distance;
-								lastDirection = "D";
-							}
-							break;
-				case "U":	if (newDirection.equals("R")) {
-								x += distance;
-								lastDirection = "R";
-							} else {
-								x -= distance;
-								lastDirection = "L";
-							}
-							break;
-				case "D":	if (newDirection.equals("R")) {
-								x -= distance;
-								lastDirection = "L";
-							} else {
-								x += distance;
-								lastDirection = "R";
-							}
-							break;
-				default:	System.out.println("Error reading file");
-							break;		
-			}
+        String lastDirection = "U";
+        while (matcher.find() && !found) {
+            String newDirection = matcher.group(1);
+            int distance = Integer.parseInt(matcher.group(2));
+            switch (lastDirection){
+                case "R":   if (newDirection.equals("R")) {
+                                y -= distance;
+                                lastDirection = "D";
+                            } else {
+                                y += distance;
+                                lastDirection = "U";
+                            }
+                            break;
+                case "L":   if (newDirection.equals("R")) {
+                                y += distance;
+                                lastDirection = "U";
+                            } else {
+                                y -= distance;
+                                lastDirection = "D";
+                            }
+                            break;
+                case "U":   if (newDirection.equals("R")) {
+                                x += distance;
+                                lastDirection = "R";
+                            } else {
+                                x -= distance;
+                                lastDirection = "L";
+                            }
+                            break;
+                case "D":   if (newDirection.equals("R")) {
+                                x -= distance;
+                                lastDirection = "L";
+                            } else {
+                                x += distance;
+                                lastDirection = "R";
+                            }
+                            break;
+                default:    System.out.println("Error reading file");
+                            break;      
+            }
 
-			Line line = new Line(lastPoint, new Point(x, y));
-			for (Line l : array) {
-				Point aux = line.intersects(l);
-				if (aux != null) {
-					found = true;	
-					System.out.println("Shortest path to the destination: " + aux.distanceTo(origin));	
-					break;
-				}
-			}
+            Line line = new Line(lastPoint, new Point(x, y));
+            for (Line l : array) {
+                Point aux = line.intersects(l);
+                if (aux != null) {
+                    found = true;   
+                    System.out.println("Shortest path to the destination: " + aux.distanceTo(origin));  
+                    break;
+                }
+            }
 
-			lastPoint = new Point(x, y);
-			array.add(line);
-		}
-	}
+            lastPoint = new Point(x, y);
+            array.add(line);
+        }
+    }
 }
