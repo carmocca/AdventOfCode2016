d224b068049a948a66aeb3ed5ad6b7d2598d9216
carmocca <carlossmocholi@hotmail.com>
2016-12-04 02:00:00 +0100

    Fixed indentation

--- Day02/Keypad_2.java
+++ Day02/Keypad_2.java
@@ -1,96 +1,96 @@
 public class Keypad_2 {
-	private char [][] keypad;
-	private int ypos, xpos;
+    private char [][] keypad;
+    private int ypos, xpos;
 
-	public Keypad_2 (char startPos) {
-		keypad = new char[][]{{'.','.','1','.','.'},
-							  {'.','2','3','4','.'},
-							  {'5','6','7','8','9'},
-							  {'.','A','B','C','.'},
-							  {'.','.','D','.','.'}};
-		buttonToPos(startPos);
-	}
+    public Keypad_2 (char startPos) {
+        keypad = new char[][]{{'.','.','1','.','.'},
+                              {'.','2','3','4','.'},
+                              {'5','6','7','8','9'},
+                              {'.','A','B','C','.'},
+                              {'.','.','D','.','.'}};
+        buttonToPos(startPos);
+    }
 
-	public char getCurrentButton(){
-		return posToButton(ypos, xpos);
-	}
+    public char getCurrentButton(){
+        return posToButton(ypos, xpos);
+    }
 
-	public void move(char move)	{
-		switch (move) {
-			case 'U':	if (ypos > 0 && keypad[ypos - 1][xpos] != '.') {
-							ypos--;
-						}
-						break;
-			case 'D':	if (ypos < 4 && keypad[ypos + 1][xpos] != '.') {
-							ypos++;
-						}
-						break;
-			case 'R':	if (xpos < 4 && keypad[ypos][xpos + 1] != '.') {
-							xpos++;
-						}
-						break;
-			case 'L':	if (xpos > 0 && keypad[ypos][xpos - 1] != '.') {
-							xpos--;
-						}
-						break;
-			default: 	System.out.println("Error: Wrong move");
-						System.exit(0);
-						break;
-		}
-	}
+    public void move(char move) {
+        switch (move) {
+            case 'U':   if (ypos > 0 && keypad[ypos - 1][xpos] != '.') {
+                            ypos--;
+                        }
+                        break;
+            case 'D':   if (ypos < 4 && keypad[ypos + 1][xpos] != '.') {
+                            ypos++;
+                        }
+                        break;
+            case 'R':   if (xpos < 4 && keypad[ypos][xpos + 1] != '.') {
+                            xpos++;
+                        }
+                        break;
+            case 'L':   if (xpos > 0 && keypad[ypos][xpos - 1] != '.') {
+                            xpos--;
+                        }
+                        break;
+            default:    System.out.println("Error: Wrong move");
+                        System.exit(0);
+                        break;
+        }
+    }
 
-	/* Given the key modifies the position
-	*/
-	public void buttonToPos(char startPos) {
-		switch (startPos) {
-			case '1':	ypos = 0;
-						xpos = 2;
-						break;
-			case '2':	ypos = 1;
-						xpos = 1;
-						break;
-			case '3':	ypos = 1;
-						xpos = 2;
-						break;
-			case '4':	ypos = 1;
-						xpos = 3;
-						break;
-			case '5':	ypos = 2;
-						xpos = 0;
-						break;
-			case '6':	ypos = 2;
-						xpos = 1;
-						break;
-			case '7':	ypos = 2;
-						xpos = 2;
-						break;
-			case '8':	ypos = 2;
-						xpos = 3;
-						break;
-			case '9':	ypos = 2;
-						xpos = 4;
-						break;
-			case 'A':	ypos = 3;
-						xpos = 1;
-						break;
-			case 'B':	ypos = 3;
-						xpos = 2;
-						break;
-			case 'C':	ypos = 3;
-						xpos = 3;
-						break;
-			case 'D':	ypos = 4;
-						xpos = 2;
-						break;
-			default:	ypos = 2; //Center
-						xpos = 2;
-						break;
-		}
-	}
+    /* Given the key modifies the position
+    */
+    public void buttonToPos(char startPos) {
+        switch (startPos) {
+            case '1':   ypos = 0;
+                        xpos = 2;
+                        break;
+            case '2':   ypos = 1;
+                        xpos = 1;
+                        break;
+            case '3':   ypos = 1;
+                        xpos = 2;
+                        break;
+            case '4':   ypos = 1;
+                        xpos = 3;
+                        break;
+            case '5':   ypos = 2;
+                        xpos = 0;
+                        break;
+            case '6':   ypos = 2;
+                        xpos = 1;
+                        break;
+            case '7':   ypos = 2;
+                        xpos = 2;
+                        break;
+            case '8':   ypos = 2;
+                        xpos = 3;
+                        break;
+            case '9':   ypos = 2;
+                        xpos = 4;
+                        break;
+            case 'A':   ypos = 3;
+                        xpos = 1;
+                        break;
+            case 'B':   ypos = 3;
+                        xpos = 2;
+                        break;
+            case 'C':   ypos = 3;
+                        xpos = 3;
+                        break;
+            case 'D':   ypos = 4;
+                        xpos = 2;
+                        break;
+            default:    ypos = 2; //Center
+                        xpos = 2;
+                        break;
+        }
+    }
 
-	/*	Given the position on the matrix returns the key number
-	*/
-	public char posToButton(int y, int x) {
-		return keypad[y][x];
-	}	
+    /*  Given the position on the matrix returns the key number
+    */
+    public char posToButton(int y, int x) {
+        return keypad[y][x];
+    }   
 }
