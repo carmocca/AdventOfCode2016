e88594ca277b9d29c81193d1a2d86219615b64cd
carmocca <carlossmocholi@hotmail.com>
2016-12-06 00:02:07 +0100

    Added day 5

---
+++ Day05/Day5.java
@@ -0,0 +1,43 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.io.UnsupportedEncodingException;
+
+public class Day5 {
+	
+	public static void main (String[] args) {
+		
+		String input = "ugkcyxxp";
+		String res = "";
+		
+		int j = 0;
+		while (res.length() < 8) {
+			String md5 = getMD5(input + j);
+			if (md5.startsWith("00000")) {
+				res += md5.charAt(5);
+			}
+			j++;
+		}
+
+		System.out.println("The password is: " + res);
+	}
+
+	/*	From https://www.mkyong.com/java/java-md5-hashing-example/
+	*/
+	public static String getMD5(String s) {
+		
+		byte[] hashedBytes = null;
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+	        hashedBytes = md.digest(s.getBytes("UTF-8"));
+	    } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+	    	System.out.println("Exception in function getMD5");
+	    }
+
+	    StringBuffer sb = new StringBuffer();
+	    for (int i = 0; i < hashedBytes.length; i++) {
+	        sb.append(Integer.toString((hashedBytes[i] & 0xff) + 0x100, 16)
+	                .substring(1));
+	    }
+	    return sb.toString();
+	}
+}
---
+++ Day05/Day5_2.java
@@ -0,0 +1,48 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.io.UnsupportedEncodingException;
+import java.lang.StringBuilder;
+
+public class Day5_2 {
+	
+	public static void main (String[] args) {
+		
+		String input = "ugkcyxxp";
+		StringBuilder res = new StringBuilder("________");
+		
+		int j = 0;
+		while (res.indexOf("_") != -1) {
+			String md5 = getMD5(input + j);
+			if (md5.startsWith("00000")) {
+				int position = Character.getNumericValue(md5.charAt(5));
+				if (position >= 0 && position < 8 && res.charAt(position) == '_') {
+					System.out.println(res.toString()); // For visualization
+					res.setCharAt(position, md5.charAt(6));
+				}
+			}
+			j++;
+		}
+
+		System.out.println("The password is: " + res);
+	}
+
+	/*	From https://www.mkyong.com/java/java-md5-hashing-example/
+	*/
+	public static String getMD5(String s) {
+		
+		byte[] hashedBytes = null;
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+	        hashedBytes = md.digest(s.getBytes("UTF-8"));
+	    } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+	    	System.out.println("Exception in function getMD5");
+	    }
+
+	    StringBuffer sb = new StringBuffer();
+	    for (int i = 0; i < hashedBytes.length; i++) {
+	        sb.append(Integer.toString((hashedBytes[i] & 0xff) + 0x100, 16)
+	                .substring(1));
+	    }
+	    return sb.toString();
+	}
+}
