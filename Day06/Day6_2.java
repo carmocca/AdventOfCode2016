84d38fdd1c191f837bd014a25c6fc6d33bbef372
carmocca <carlossmocholi@hotmail.com>
2016-12-06 14:28:57 +0100

    Added day 6

---
+++ Day06/Day6_2.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day6_2 {
+
+    public static List<Map<Character, Integer>> log = new ArrayList<Map<Character, Integer>>();
+
+    public static void main (String[] args) {
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new File("input.txt"));
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+            System.exit(0);
+        }
+
+        // Create maps
+        for (int i = 0; i < 8; i++) {
+            log.add(new HashMap<Character, Integer>());
+        }
+
+        // Process the input
+        while(sc.hasNext()) {
+            String input = sc.nextLine();
+            for (int i = 0; i < input.length(); i++) {
+                Integer num = log.get(i).get(input.charAt(i));
+                log.get(i).put(input.charAt(i), (num == null ? 1 : ++num));
+            }
+        }
+
+        // Get the message by finding the minimum for each map
+        String msg = "";
+        for (Map<Character, Integer> map : log) {
+            int min = (int) Double.POSITIVE_INFINITY;
+            char ch = 'X';
+            for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+                Character key = entry.getKey();
+                Integer value = entry.getValue();
+                if (min >= value) {
+                    min = value;
+                    ch = key;
+                }
+            }
+            msg += ch;
+        }
+        System.out.println("The original message is: " + msg);
+    }
+}
