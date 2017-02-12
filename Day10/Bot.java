8459857cc0730ec092ca414b7b0a372a903790b9
carmocca <carlossmocholi@hotmail.com>
2017-02-12 18:41:48 +0100

    Added day 10

---
+++ Day10/Bot.java
@@ -0,0 +1,59 @@
+package Day10;
+
+public class Bot {
+    private int id;
+    private int[] chips;
+
+    public Bot(int id) {
+        this.id = id;
+        chips = new int[2];
+    }
+
+    public void addChip(int chip) {
+        if (chips[0] == 0) {
+            chips[0] = chip;
+        } else if (chips[1] == 0) {
+            chips[1] = chip;
+        } else {
+            System.err.println("Bot " + id + "cannot handle more chips");
+        }
+    }
+
+    public int removeLowestChip() {
+        int chip;
+        if (chips[0] < chips[1]) {
+            chip = chips[0];
+            chips[0] = 0;
+        } else {
+            chip = chips[1];
+            chips[1] = 0;
+        }
+        return chip;
+    }
+
+    public int removeHighestChip() {
+        int chip;
+        if (chips[0] > chips[1]) {
+            chip = chips[0];
+            chips[0] = 0;
+        } else {
+            chip = chips[1];
+            chips[1] = 0;
+        }
+        return chip;
+    }
+
+    public boolean hasTwoChips() {
+        return (chips[0] != 0 && chips[1] != 0);
+    }
+
+    public boolean isResponsible(int chip1, int chip2) {
+        return ((chips[0] == chip1 && chips[1] == chip2) ||
+                (chips[0] == chip2 && chips[1] == chip1));
+    }
+
+    @Override
+    public String toString() {
+        return "Bot: " + id + " {" + chips[0] + ", " + chips[1] + "}";
+    }
+}
---
+++ Day10/Day10.java
@@ -0,0 +1,96 @@
+package Day10;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day10 {
+    public static Bot[] botArray;
+    public static int[] outputArray;
+
+    public static void main(String[] args) {
+        String input = null;
+        try {
+            input = new String(Files.readAllBytes(Paths.get("Day10/input.txt")));
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+            System.exit(0);
+        }
+
+        botArray = new Bot[210];
+        outputArray = new int[21];
+
+        String regex = "value (\\d+) goes to bot (\\d+)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        // Give starting chips to bots
+        while (matcher.find()) {
+            int botId = Integer.parseInt(matcher.group(2));
+            if (botArray[botId] == null) {
+                botArray[botId] = new Bot(botId);
+            }
+            botArray[botId].addChip(Integer.parseInt(matcher.group(1)));
+        }
+
+        // Initial search for the responsible one (As in the example case)
+        findResponsible();
+
+        //Create the rest of the bots
+        for (int i = 0; i < botArray.length; i++) {
+            if (botArray[i] == null) {
+                botArray[i] = new Bot(i);
+            }
+        }
+
+        regex = "bot (\\d+) gives low to (\\w+) (\\d+) and high to (\\w+) (\\d+)";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(input);
+
+        // Process the instructions, in this part we can ignore the output bins
+        while (matcher.find()) {
+            int id1 = Integer.parseInt(matcher.group(1));
+            int id2 = Integer.parseInt(matcher.group(3));
+            int id3 = Integer.parseInt(matcher.group(5));
+            Bot givingBot = botArray[id1];
+            if (givingBot.hasTwoChips()) {
+                int lowestChip = givingBot.removeLowestChip();
+                int highestChip = givingBot.removeHighestChip();
+                botArray[id1] = givingBot;
+                if (!matcher.group(2).equals("output")) {
+                    Bot lowReceivingBot = botArray[id2];
+                    lowReceivingBot.addChip(lowestChip);
+                    botArray[id2] = lowReceivingBot;
+                } else {
+                    outputArray[id2] = lowestChip;
+                }
+                if (!matcher.group(4).equals("output")) {
+                    Bot highReceivingBot = botArray[id3];
+                    highReceivingBot.addChip(highestChip);
+                    botArray[id3] = highReceivingBot;
+                } else {
+                    outputArray[id3] = lowestChip;
+                }
+                findResponsible();
+                // Remove the processed instruction
+                matcher = pattern.matcher(input.replace(matcher.group(0), ""));
+            }
+        }
+
+        System.out.println(outputArray[0] * outputArray[1] * outputArray[2]);
+    }
+
+    public static void findResponsible(){
+        for (int i = 0; i < botArray.length ; i++) {
+            if (botArray[i] != null) {
+                if (botArray[i].isResponsible(61, 17)) {
+                    System.out.println("Responsible " + botArray[i].toString());
+                }
+            } else {
+                break;
+            }
+        }
+    }
+}
